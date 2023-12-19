964f7d91d9ef90df59c375c4792c03169abdaa3b
mbconcede <149971006+mbconcede@users.noreply.github.com>
2023-12-20 01:53:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,26 @@
 import streamlit as st
-import os
+import pandas as pd
 
-def save_uploaded_file(uploaded_file):
-    with open(os.path.join("./", uploaded_file.name), "wb") as f:
-        f.write(uploaded_file.getbuffer())
+# Загрузка файла
+uploaded_file = st.file_uploader("Загрузите ваш файл", type="csv")
 
-def main():
-    # Set page configuration
-    st.set_page_config(
-        page_title="Загрузка MP3 файла",
-        page_icon="🎵",
-        layout="wide",
-        initial_sidebar_state="expanded",
-    )
+# Выбор языка вывода
+language = st.selectbox(
+   'Выберите язык вывода',
+   ('Русский', 'Английский')
+)
 
-    # Custom CSS for background image
-    st.markdown(
-        """
-        <style>
-        body {
-            background-image: url('https://example.com/background_image.jpg');
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+# Обработка файла
+if uploaded_file is not None:
+   data = pd.read_csv(uploaded_file)
+   st.write(data)
 
-    st.title("Конспектор ^_^")
+# Кнопка "Пуск"
+if st.button('Пуск'):
+   # Здесь вы можете добавить код для обработки данных в выбранном языке
+   st.write('Обработка данных...')
 
-    uploaded_file = st.file_uploader("Выберите MP3 файл", type=["mp3"])
-
-    if uploaded_file is not None:
-        save_uploaded_file(uploaded_file)
-        st.success("Файл успешно загружен!")
-
-if __name__ == "__main__":
-    main()
+# Кнопка для скачивания результата
+if st.button('Скачать результат'):
+   # Здесь вы можете добавить код для генерации и скачивания файла
+   st.write('Генерация файла...')
