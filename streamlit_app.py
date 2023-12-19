fba79e2ca7455ff145681281be1229f7b176f10e
mbconcede <149971006+mbconcede@users.noreply.github.com>
2023-12-20 02:05:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import pandas as pd
 import os
 
+# Загрузка файла
 def save_uploaded_file(uploaded_file):
     with open(os.path.join("./", uploaded_file.name), "wb") as f:
         f.write(uploaded_file.getbuffer())
@@ -37,3 +39,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+# Выбор языка вывода
+language = st.selectbox(
+   'Выберите язык вывода',
+   ('Русский', 'Английский', 'Французский')
+)
+
+# Кнопка "Пуск"
+if st.button('Пуск'):
+   # Здесь вы можете добавить код для обработки данных в выбранном языке
+   st.write('Обработка данных...')
+
+# Кнопка для скачивания результата
+if st.button('Скачать результат'):
+   # Здесь вы можете добавить код для генерации и скачивания файла
+   st.write('Генерация файла...')
