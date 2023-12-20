d09b47cad3721bb0cd039c451688181ec2c305e1
AndreyKondratyev <149971006+mbconcede@users.noreply.github.com>
2023-12-20 22:07:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import pandas as pd
 import os
 
-# Загрузка файла
 def save_uploaded_file(uploaded_file):
-    with open(os.path.join("./", uploaded_file.name), "wb") as f:
+    with open(os.path.join("./samples", uploaded_file.name), "wb") as f:
         f.write(uploaded_file.getbuffer())
 
 def main():
@@ -37,8 +35,48 @@ def main():
         save_uploaded_file(uploaded_file)
         st.success("Файл успешно загружен!")
 
+    import subprocess
+
+    def run_file():
+        import time
+        start_time = time.time()
+        file_path = './model.py'
+        end_time = time.time()
+        subprocess.run(['python', file_path])
+
+        end_time = time.time()
+        execution_time = end_time - start_time
+        hours = int(execution_time // 3600)
+        minutes = int((execution_time % 3600) // 60)
+        seconds = int(execution_time % 60)
+        print(f"Время выполнения скрипта: {hours} часов, {minutes} минут, {seconds} секунд")
+
+    st.button('Запустить файл', on_click=run_file)
+
+    print("Ещё секунду Вашего терпения :)")
+
+    st.title("Загрузка и скачивание файла")
+    file_path = './samples/result.txt'
+    if file_path:
+        if os.path.isfile(file_path):
+            # Чтение содержимого файла
+            with open(file_path, "r") as file:
+                file_contents = file.read()
+
+            # Отображение содержимого файла на странице
+            st.text("Содержимое файла:")
+            st.text(file_contents[0:1000])
+
+            # Скачивание файла
+            st.download_button("Скачать файл", data=file_contents, file_name=os.path.basename(file_path))
+        else:
+            st.error("Файл не найден. Пожалуйста, проверьте путь к файлу.")
+
+
+
 if __name__ == "__main__":
     main()
+    
 # Выбор языка вывода
 language = st.selectbox(
    'Выберите язык вывода',
